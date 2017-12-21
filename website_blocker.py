988e2d8a9b98ee21a1954256b9f6d7c10c6cd301
Travis Young <t.w.young.ubc@gmail.com>
2017-12-20 18:59:04 -0800

    add websites when within working hours

--- website_blocker.py
+++ website_blocker.py
@@ -1,4 +1,33 @@
-
+import time
+from datetime import datetime as dt
+hosts_temp="./hosts"
 hosts_path="/etc/hosts"
 redirect="127.0.0.1"
-websites=["www.facebook.com", "facebook.com"]
+websites_list=["www.facebook.com", "facebook.com", "www.lifehacker.com", "lifehacker.com"]
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 23) :
+        print("Working Hours")
+        with open(hosts_temp, 'r+') as file:
+            content=file.read()
+            for website in websites_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+website+"\n")
+
+    else:
+        with open(hosts_temp, 'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in websites_list):
+                    file.write(line)
+            file.write('testt')
+            file.truncate()
+        print("not working hors")
+
+
+
+    time.sleep(5)
+
